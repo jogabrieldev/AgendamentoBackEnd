51c360ee13cdd573de19eedddc1829da0202010f
SeuNomeDeUsuário <joaog4030@gmail.com>
2025-09-03 10:38:13 -0300

    retirando o service de whatsapp para testar outras rotas

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ import express from 'express'
 import dataBase  from './src/models/initModels.js'
 import './src/models/associations.js'
 import router from './src/routes/routes.js'
-import { connectToWhatsApp } from './src/services/whatsappService.js'
+// import { connectToWhatsApp } from './src/services/whatsappService.js'
 import cors from 'cors'
 
 const app = express()
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(204);
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(cors({
   origin:["https://agendamento-vert.vercel.app/login"],
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -26,11 +26,11 @@ app.use(express.json())
 
 app.use(router)
 
-try {
-  connectToWhatsApp();
-} catch (err) {
-  console.error("❌ Erro ao conectar ao WhatsApp:", err);
-}
+// try {
+//   connectToWhatsApp();
+// } catch (err) {
+//   console.error("❌ Erro ao conectar ao WhatsApp:", err);
+// }
 
 const PORT = process.env.PORT || 3000;
 
