f296e2841bd93720b915d51627f4bc73ffa58b1d
SeuNomeDeUsuário <joaog4030@gmail.com>
2025-09-03 15:35:43 -0300

    no cors manual agora

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 const app = express()
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "https://agendamento-vert.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
