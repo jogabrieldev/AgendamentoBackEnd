093195f492cf79d51032704e2b91f3d932841ade
SeuNomeDeUsuário <joaog4030@gmail.com>
2025-10-08 09:29:09 -0300

    Retirando o force para garantir que os dados armazene corretamente no banco de dados.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use(router)
 
 const createTableDataBase = () => {
-  dataBase.sequelize.sync({ force: true }) 
+  dataBase.sequelize.sync({ alter: true }) 
     .then(() => {
       console.log('✅ Conectado ao banco e tabelas sincronizadas');
     })
