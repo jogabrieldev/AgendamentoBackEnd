228ce11be2ae0c2ab3430aa7c4599bc61e254841
SeuNomeDeUsuário <joaog4030@gmail.com>
2025-10-07 21:11:59 -0300

    Retirando o force ara voltar ao normal minha aplicação.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(router)
 
 const createTableDataBase = () => {
-  dataBase.sequelize.sync({ force: true }) 
+  dataBase.sequelize.sync({ alter: true }) 
     .then(() => {
       console.log('✅ Conectado ao banco e tabelas sincronizadas');
     })
